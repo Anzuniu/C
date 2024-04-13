21341cf4894ccf7650066d78925965292c0812ca
Antonio <antonio@DESKTOP-3M8LIQ9>
2024-04-13 12:54:40 +0100

    ft_strlcat.c_Fixed

--- Cursus/circle_0/Libft/ft_strlcat.c
+++ Cursus/circle_0/Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: antalvar <antalvar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 02:44:31 by antalvar          #+#    #+#             */
-/*   Updated: 2024/04/13 01:25:09 by antonio          ###   ########.fr       */
+/*   Updated: 2024/04/13 12:54:00 by antonio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,27 @@ size_t	ft_strlcat(char	*sr1, const char *sr2, size_t size)
 {
 	size_t	i;
 	size_t	j;
-	size_t	len_total;
+	size_t	sr1_len;
+	size_t	sr2_len;
 
-	i = 0;
+	sr1_len = 0;
+	sr2_len = 0;
+	while (sr1[sr1_len] && sr1_len < size)
+		sr1_len++;
+	while (sr2[sr2_len])
+		sr2_len++;
+	if (sr1_len == size)
+		return (sr1_len + sr2_len);
+	i = sr1_len;
 	j = 0;
-	while (sr1[i])
-		i++;
-	while (sr2[j])
-		j++;
-	len_total = i + j;
-	j = 0;
-	while (sr2[j] && i < size)
+	while (sr2[j] && i < size - 1)
 	{
 		sr1[i] = sr2[j];
 		i++;
 		j++;
 	}
-	sr1[i] = 0;
-	if (len_total > size)
-		return (-1);
-	else
-		return (i);
+	sr1[i] = '\0';
+	return (sr1_len + sr2_len);
 }
 
 /*int main()
