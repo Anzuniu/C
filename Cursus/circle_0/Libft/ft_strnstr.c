c3fd1872682fc0d485179b5cf9473bf8f3c30130
Antonio <antonio@DESKTOP-3M8LIQ9>
2024-04-14 01:44:27 +0100

    ft_strnstr.c

--- Cursus/circle_0/Libft/ft_strnstr.c
+++ Cursus/circle_0/Libft/ft_strnstr.c
@@ -6,21 +6,25 @@
 /*   By: antalvar <antalvar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 02:45:21 by antalvar          #+#    #+#             */
-/*   Updated: 2024/04/12 03:02:06 by antonio          ###   ########.fr       */
+/*   Updated: 2024/04/14 01:42:46 by antonio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t size)
+/*char	*ft_strnstr(const char *haystack, const char *needle, size_t size)
 {
 	size_t	i;
 	size_t	j;
+	size_t	needle_len;
 
+	if (*needle == '\0')
+		return ((char *)haystack);
 	i = 0;
 	while (needle[i])
 		i++;
-	if (size < i)
+	needle_len = i;
+	if (size < needle_len)
 		return (NULL);
 	i = 0;
 	j = 0;
@@ -36,6 +40,27 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t size)
 			return ((char *)&haystack[i - j]);
 		j = 0;
 		i++;
+	}
+	return (NULL);
+}*/
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t size)
+{
+	size_t	i;
+    	size_t needle_len;
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+	needle_len = ft_strlen(needle);
+	while (*haystack && size >= needle_len)
+	{
+		i = 0;
+		while (haystack[i] == needle[i] && haystack[i] && needle[i])
+			i++;
+		if (i == needle_len)
+			return ((char *)haystack);
+		haystack++;
+		size--;
 	}
 	return (NULL);
 }
