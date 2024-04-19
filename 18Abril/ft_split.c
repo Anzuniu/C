0737b74a865f28afd1bfd935ee5e726543bc12af
Marina Barba <marina.barbaruiz@king.com>
2024-04-19 02:33:20 +0200

    @ft_split@

--- 18Abril/ft_split.c
+++ 18Abril/ft_split.c
@@ -1,105 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: antalvar <antalvar@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 02:43:51 by antalvar          #+#    #+#             */
-/*   Updated: 2024/04/18 15:52:39 by antalvar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-//Splits a string into others strings.
-
 #include "libft.h"
 
-char	**ft_split(char const *s, char c);
-void	free_s(char **s);
-
-/*int main()
+size_t	ft_counterchar(char const *s, char c)
 {
-	char const	s[] = "Como estan los maquinas, lo primero de todo.";
-	char	c = 'm';
-	char	**split = ft_split(s,c);
-    int i = 0;
+	size_t	i;
+	size_t	x;
 
-    while(split[i] != NULL)
-        printf("%s\n",split[i++]);
-    free_s(split);
-}*/
+	i = 0;
+	x = 0;
+	while (s[i])
+	{
+		if (s[i++] != c && (s[i] == c || s[i] == 0))
+			x++;
+	}
+	return (x);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	char	**split;
-	char	**tmp;
-	size_t	i;
-	size_t	j;
-	size_t	start;
+	char	**str;
 	size_t	len;
-	size_t	num_strings;
-	char	*substr;
+	size_t	n;
 
-	split = NULL;
-	i = 0;
-	start = 0;
-	num_strings = 0;
-	if (!s)
-		return (NULL);
-	while (s[i])
+	str = malloc (sizeof (char *) * (ft_counterchar(s, c) + 1));
+	if (!str)
+		return (0);
+	n = 0;
+	while (*s)
 	{
-		if (s[i] == c || s[i + 1] == '\0')
+		if (*s != c)
 		{
-			if (s[i] == c && s[i + 1] != c)
-				len = i - start;
-			else
-				len = i - start + 1;
-			substr = ft_substr(s, start, len);
-			
-			//CHECKEO DE MEMORIA DE SUBSTR
-			if (substr == NULL)
+			len = 0;
+			while (*s && *s != c)
 			{
-				free_s(split);
-				return (NULL);
+				++len;
+				++s;
 			}
-			
-			tmp = (char **)malloc((num_strings + 2) * sizeof(char *));
-			
-			//CHECKEO DE MEMORIA DE TMP
-			if (tmp == NULL)
-			{
-				free(substr);
-				free_s(split);
-				return (NULL);
-			}
-
-			j = 0;
-			while (j < num_strings)
-			{
-				tmp[j] = split[j];
-				j++;
-			}
-
-
-			tmp[num_strings++] = substr;
-			tmp[num_strings] = NULL;
-			free(split);
-			split = tmp;
-			start = i + 1;
+			str[n++] = ft_substr(s - len, 0, len);
 		}
-		i++;
+		else
+			++s;
 	}
-	return (split);
-}
-
-void	free_s(char **s)
-{
-	int	i;
-
-	i = 0;
-	if (s == NULL)
-		return ;
-	while (s[i] != NULL)
-		free(s[i++]);
-	free(s);
+	str[n] = 0;
+	return (str);
 }
