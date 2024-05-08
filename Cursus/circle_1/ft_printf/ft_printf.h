494fab2f9fed4bc7111bffc317d6ed35ee845c44
Antonio <antonio@DESKTOP-3M8LIQ9>
2024-05-08 04:22:32 +0100

    Hexa Update

--- Cursus/circle_1/ft_printf/ft_printf.h
+++ Cursus/circle_1/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: antalvar <antalvar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 15:51:38 by antalvar          #+#    #+#             */
-/*   Updated: 2024/05/08 01:38:15 by antalvar         ###   ########.fr       */
+/*   Updated: 2024/05/08 04:16:38 by antalvar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int		ft_putchar(char c);
 int		ft_putdeci(int n);
 int		ft_putstr(char *s);
 int		ft_putunnbr(unsigned int i);
-int		ft_puthex(int n, char c);
-int		ft_puthex_format(int n, char c);
+int		ft_puthex(unsigned long n, char c);
+int		ft_puthex_format(unsigned long n, char c);
 int		ft_printf(const char *format, ...);
 int		ft_check_format(const char *str, va_list args);
 int		ft_putunnbr(unsigned int n);
--- Cursus/circle_1/ft_printf/ft_puthex.c
+++ Cursus/circle_1/ft_printf/ft_puthex.c
@@ -6,35 +6,30 @@
 /*   By: antalvar <antalvar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 01:25:33 by antalvar          #+#    #+#             */
-/*   Updated: 2024/05/08 02:37:42 by antonio          ###   ########.fr       */
+/*   Updated: 2024/05/08 04:20:54 by antonio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_puthex(int n, char c)
+int	ft_puthex(unsigned long n, char c)
 {
 	char	aux;
-	int		i;
+	int		printed_chars;
 
 	aux = c;
-	i = 0;
-	if (n < 0)
-	{
-		i += ft_putchar('-');
-		i += ft_puthex(-n, c);
-	}
+	printed_chars = 0;
 	if (n >= 16)
 	{
-		i += ft_puthex((n / 16), aux);
-		i += ft_puthex((n % 16), aux);
+		printed_chars += ft_puthex(((unsigned int)n / 16), aux);
+		printed_chars += ft_puthex(((unsigned int)n % 16), aux);
 	}
 	else
-		i += ft_puthex_format(n, aux);
-	return (i);
+		printed_chars += ft_puthex_format(n, aux);
+	return (printed_chars);
 }
 
-int	ft_puthex_format(int n, char c)
+int	ft_puthex_format(unsigned long n, char c)
 {
 	char	*hex_up;
 	char	*hex_low;
@@ -43,15 +38,9 @@ int	ft_puthex_format(int n, char c)
 	printed_chars = 0;
 	hex_up = "0123456789ABCDEF";
 	hex_low = "0123456789abcdef";
-	if (c == 'x')
-	{
-		ft_putchar(hex_low[n]);
-		printed_chars++;
-	}
+	if (c == 'x' || c == 'p')
+		printed_chars += ft_putchar(hex_low[n]);
 	if (c == 'X')
-	{
-		ft_putchar(hex_up[n]);
-		printed_chars++;
-	}
+		printed_chars += ft_putchar(hex_up[n]);
 	return (printed_chars);
 }
