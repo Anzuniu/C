9d7066c554264cf29839db694b17b679d3699462
Anzuniu <158233603+Anzuniu@users.noreply.github.com>
2024-05-15 01:23:58 +0200

    Update sript_born2beroot.c

--- sript_born2beroot.c
+++ sript_born2beroot.c
@@ -1,81 +1,47 @@
 #!/bin/bash
-
-# ARCH
-arch=$(uname -a)
-
+//--------------------------------------------------------------------------------------------------------------------------
+# ARCHITETURE
+	arc=$(uname -a)
+//--------------------------------------------------------------------------------------------------------------------------
 # CPU PHYSICAL
-cpuf=$(grep "physical id" /proc/cpuinfo | wc -l)
-
+	pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l) 
+//--------------------------------------------------------------------------------------------------------------------------
 # CPU VIRTUAL
-cpuv=$(grep "processor" /proc/cpuinfo | wc -l)
-
+	vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+//--------------------------------------------------------------------------------------------------------------------------
 # RAM
-ram_total=$(free --mega | awk '$1 == "Mem:" {print $2}')
-ram_use=$(free --mega | awk '$1 == "Mem:" {print $3}')
-ram_percent=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-
+	fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+	uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+	pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+//--------------------------------------------------------------------------------------------------------------------------
 # DISK
-disk_total=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_t += $2} END {printf ("%.1fGb\n"), disk_t/1024}')
-disk_use=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
-disk_percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%d"), disk_u/disk_t*100}')
-
+	fdisk=$(df -BG | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
+	udisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
+	pdisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+//--------------------------------------------------------------------------------------------------------------------------
 # CPU LOAD
-cpul=$(vmstat 1 2 | tail -1 | awk '{printf $15}')
-cpu_op=$(expr 100 - $cpul)
-cpu_fin=$(printf "%.1f" $cpu_op)
-
+	cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+//--------------------------------------------------------------------------------------------------------------------------
 # LAST BOOT
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-
+	lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+//--------------------------------------------------------------------------------------------------------------------------
 # LVM USE
-lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
-
-# TCP CONNEXIONS
-tcpc=$(ss -ta | grep ESTAB | wc -l)
-
+	lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+//--------------------------------------------------------------------------------------------------------------------------
+# TCP CONNECTIONS
+	ctcp=$(ss -neopt state established | wc -l)
+//--------------------------------------------------------------------------------------------------------------------------
 # USER LOG
-ulog=$(users | wc -w)
-
+	ulog=$(users | wc -w)
+//--------------------------------------------------------------------------------------------------------------------------
 # NETWORK
-ip=$(hostname -I)
-mac=$(ip link | grep "link/ether" | awk '{print $2}')
-
+	ip=$(hostname -I)
+	mac=$(ip link show | grep "ether" | awk '{print $2}')
+//---------------------------------------------------------------------------------------------------------------------------
 # SUDO
-cmnd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-
-wall "	Architecture: $arch
-	CPU physical: $cpuf
-	vCPU: $cpuv
-	Memory Usage: $ram_use/${ram_total}MB ($ram_percent%)
-	Disk Usage: $disk_use/${disk_total} ($disk_percent%)
-	CPU load: $cpu_fin%
-	Last boot: $lb
-	LVM use: $lvmu
-	Connections TCP: $tcpc ESTABLISHED
-	User log: $ulog
-	Network: IP $ip ($mac)
-	Sudo: $cmnd cmd"
-
-
-
-#!/bin/bash
-arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l) 
-vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -BG | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
-ctcp=$(ss -neopt state established | wc -l)
-ulog=$(users | wc -w)
-ip=$(hostname -I)
-mac=$(ip link show | grep "ether" | awk '{print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+	cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+//---------------------------------------------------------------------------------------------------------------------------
+	
 wall "	#Architecture: $arc
 	#CPU physical: $pcpu
 	#vCPU: $vcpu
